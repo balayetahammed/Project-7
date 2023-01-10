5463e5a8c7e984ff136d242334db529c7b591333
balayetahammed <balayetahammedrifat@gmail.com>
2023-01-11 01:25:06 +0600

    project-7

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(".main_slide").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: '<span class="prev"><i class="fa-solid fa-arrow-left"></i></span>',
+  nextArrow:
+    '<span class="next"><i class="fa-solid fa-arrow-right"></i></span>',
+});
